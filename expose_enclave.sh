00ab91e335219863deb2909d78de8b8c97ea6435
VietAnhLeVu <vietanhlevu2004@gmail.com>
2025-11-12 16:10:17 +0700

    testing

--- expose_enclave.sh
+++ expose_enclave.sh
@@ -9,6 +9,8 @@ ENCLAVE_CID=$(nitro-cli describe-enclaves | jq -r ".[0].EnclaveCID")
 
 sleep 5
 # Secrets-block
+SECRET_VALUE=$(aws secretsmanager get-secret-value --secret-id weather-api-key --region ap-southeast-2 | jq -r .SecretString)
+echo "$SECRET_VALUE" | jq -R '{"API_KEY": .}' > secrets.json
 # This section will be populated by configure_enclave.sh based on secret configuration
 
 cat secrets.json | socat - VSOCK-CONNECT:$ENCLAVE_CID:7777
